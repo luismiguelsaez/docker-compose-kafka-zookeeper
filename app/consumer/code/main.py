98aa8e2e1b2599f8152ebf178218e15f68e19f3e
luismiguelsaez <luismiguelsaez83@gmail.com>
2020-11-27 09:22:40 +0100

    Modified consumer app code

--- app/consumer/code/main.py
+++ app/consumer/code/main.py
@@ -1,15 +1,10 @@
 from kafka import KafkaConsumer
-from pymongo import MongoClient
-from json import loads
 
-consumer = KafkaConsumer(
-            'numtest',
-            bootstrap_servers=['kafka-1:9092','kafka-2:9092','kafka-3:9092'],
-            auto_offset_reset='earliest',
-            enable_auto_commit=True,
-            group_id='my-group',
-            value_deserializer=lambda x: loads(x.decode('utf-8'))
-           )
 
-for message in consumer:
-    print(message.value)
+kafka_bootstrap_servers = ['kafka-1:9092','kafka-2:9092','kafka-3:9092']
+kafka_topic_name = 'dbserver1.communication.messages'
+
+kafka_consumer = KafkaConsumer ( kafka_topic_name, group_id ='group1', bootstrap_servers = kafka_bootstrap_servers )
+
+for msg in kafka_consumer:
+    print("Topic Name=%s,Message=%s"%(msg.topic,msg.value))
